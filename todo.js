648f96ff2a4fc54fd8c82c91a69c76f6143dfd38
ar24git <arthurantonuyk@gmail.com>
2024-04-08 09:52:47 +0300

    v1 complete

--- todo.js
+++ todo.js
@@ -1,24 +1,33 @@
 // Κώδικας που θα εκτελείται όταν φορτωθεί η σελίδα:
 // Code that will run when the page is loaded:
 colorEveryOddTask();
-// changes the text content to total count
-const total = document.querySelector(".total");
-total.textContent = getTotalCount();
-//same procedure for left to do
-const leftToDo = document.querySelector(".left-todo");
-leftToDo.textContent = getTotalCount() - getDoneCount();
+countCalculator();
+
+function countCalculator(){
+    // changes the text content to total count
+    const total = document.querySelector(".total");
+    total.textContent = getTotalCount();
+    //same procedure for left to do
+    const leftToDo = document.querySelector(".left-todo");
+    leftToDo.textContent = getTotalCount() - getDoneCount();
+}
+
 //
 // Γράψτε εδώ τον κώδικά σας
 // Write your code here
 // remove button
-const buttons = document.querySelectorAll("button");
-const toDoList = document.querySelectorAll("li");
-for(let i=0;i<buttons.length;i++){
-    buttons[i].addEventListener("click", function(){ 
-        toDoList[i].classList.add("done");
-        const leftToDo = document.querySelector(".left-todo");
-        leftToDo.textContent = getTotalCount() - getDoneCount();
-    });
+removeFunctionality();
+
+function removeFunctionality(){
+    const buttons = document.querySelectorAll("button");
+    const toDoList = document.querySelectorAll("li");
+    for(let i=0;i<buttons.length;i++){
+        buttons[i].addEventListener("click", function(){ 
+            toDoList[i].classList.add("done");
+            const leftToDo = document.querySelector(".left-todo");
+            leftToDo.textContent = getTotalCount() - getDoneCount();
+        });
+    }
 }
 
 //input
@@ -26,13 +35,18 @@ const input  = document.getElementById("item");
 
 input.addEventListener("keyup", function(e){
     if(e.key==='Enter'){
-        alert(e);
+        //add task
+        const listItem = document.createElement("li");
+        listItem.innerHTML += "<span>"+ input.value +"</span><button>Αφαίρεση</button>";
+
+        let toDoList = document.querySelector(".task-list");
+        toDoList.appendChild(listItem);
+        input.value = "";
+        colorEveryOddTask();
+        removeFunctionality();
+        countCalculator();
     }
 });
-
-
-
-
 
 
 //--------------------------------------------------
